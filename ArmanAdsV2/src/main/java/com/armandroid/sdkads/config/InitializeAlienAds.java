786ed2d315f5ab6f608aa2366a5115dce5b0720b
ANIISS <siva.banova@gmail.com>
2022-11-19 19:10:25 +0100

    nevomber

---
+++ ArmanAdsV2/src/main/java/com/armandroid/sdkads/config/InitializeAlienAds.java
@@ -0,0 +1,14 @@
+package com.aliendroid.sdkads.config;
+
+public class InitializeAlienAds {
+    public static com.armandroid.armanads.MyApplication application;
+
+    public InitializeAlienAds(com.armandroid.armanads.MyApplication myApplication) {
+        application = myApplication;
+    }
+
+    public static void LoadSDK() {
+        Connection.SDKMediation(application, AppsConfig.APPKEY);
+    }
+
+}
